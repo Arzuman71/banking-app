8c2ffc7c4e7b0e29de715e0c5cdb15171674010a
User <arzuman.kochoyan98@mail.ru>
2025-05-21 12:26:40 +0400

    init commit

--- src/main/resources/db/migration/V1__create_tables_and_insert_users.sql
+++ src/main/resources/db/migration/V1__create_tables_and_insert_users.sql
@@ -3,7 +3,8 @@ CREATE TABLE "user"
 (
     id            BIGINT PRIMARY KEY,
     name          VARCHAR(500),
-    date_of_birth DATE password VARCHAR(500) NOT NULL CHECK (LENGTH(password) BETWEEN 8 AND 500)
+    date_of_birth DATE,
+    password      VARCHAR(500) NOT NULL CHECK (LENGTH(password) BETWEEN 8 AND 500)
 );
 
 -- Создание таблицы ACCOUNT
@@ -21,8 +22,8 @@ CREATE TABLE email_data
     id      BIGINT PRIMARY KEY,
     user_id BIGINT              NOT NULL,
     email   VARCHAR(200) UNIQUE NOT NULL CHECK (email ~ '^[^@]+@[^@]+\.[^@]+$'
-) ,
-    CONSTRAINT fk_user_email FOREIGN KEY (user_id) REFERENCES "user"(id)
+        ),
+    CONSTRAINT fk_user_email FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
 
 -- Создание таблицы PHONE_DATA
@@ -31,6 +32,6 @@ CREATE TABLE phone_data
     id      BIGINT PRIMARY KEY,
     user_id BIGINT             NOT NULL,
     phone   VARCHAR(13) UNIQUE NOT NULL CHECK (phone ~ '^7\d{10}$'
-) ,
-    CONSTRAINT fk_user_phone FOREIGN KEY (user_id) REFERENCES "user"(id)
+        ),
+    CONSTRAINT fk_user_phone FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
